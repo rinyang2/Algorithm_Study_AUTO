b3a8fb7f6b442863abb62ea3aa7f10075d8f42f5
Jinseok Yang <49273615+rinyang2@users.noreply.github.com>
2023-10-19 11:26:20 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 136 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<stack>
+#include<queue>
+using namespace std;
+
+string str;
+
+
+int main()
+{
+	while (1)
+	{
+		getline(cin, str); 
+		stack<char> a;
+		if (str[0] == '.') break;
+
+		for (int i = 0; i < str.length() - 1; i++)
+		{
+			if (str[i] == '(') a.push('(');
+			if (str[i] == '[') a.push('[');
+			if (str[i] == ']')
+			{
+				if (!a.empty() && a.top() == '[') a.pop();
+				else { printf("no\n"); break; } 
+			}
+
+			if (str[i] == ')')
+			{
+				if (!a.empty() && a.top() == '(') a.pop();
+				else { printf("no\n"); break; } 
+			}
+
+			if (a.empty() && i == str.length() - 2) printf("yes\n"); 
+			else if (!a.empty() && i == str.length() - 2) printf("no\n");
+		}
+
+	}
+}

009b6d3f0677b252a57006c99ebfc83e62704dc5
Jinseok Yang <49273615+rinyang2@users.noreply.github.com>
2023-12-23 00:53:59 +0900

    [Silver IV] Title: 로프, Time: 20 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.cc
+++ 백준/Silver/2217. 로프/로프.cc
@@ -2,7 +2,8 @@
 #include<queue>
 #include<vector>
 using namespace std;
-int main(){priority_queue<int,vector<int>,greater<>>pq;int N;cin>>N;
+int main(){ios::sync_with_stdio(false); cin.tie(NULL);
+priority_queue<int,vector<int>,greater<>>pq;int N;cin>>N;
 for(int i=0;i<N;i++){int T;cin>>T;pq.push(T);}
 int ans=0;
 while(!pq.empty()){ans=max(ans,pq.top()*N);N--;pq.pop();}

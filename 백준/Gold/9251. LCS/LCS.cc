a04686c11cf333e532bd2cd1631e9e8a51e299a9
Jinseok Yang <49273615+rinyang2@users.noreply.github.com>
2024-01-03 14:41:21 +0900

    [Gold V] Title: LCS, Time: 8 ms, Memory: 6000 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    string s1, s2;
+    cin>>s1>>s2;
+    int n=s1.length(), m=s2.length();
+    vector<vector<int>> v(n+1);
+    for(int i=0;i<n+1;i++){
+        v[i].resize(m+1);
+        if(i>0){
+            for(int j=1;j<m+1;j++){
+                if(s1[i-1]==s2[j-1])
+                    v[i][j]=v[i-1][j-1]+1;
+                else
+                    v[i][j]=max(v[i-1][j], v[i][j-1]);
+            }
+        }
+    }
+    int ans = 0;
+    for(auto a: v){
+        for(auto b: a){
+            ans = max(ans, b);
+        }
+    }
+    cout<<ans;
+}

6429a1bd9f9a944904aa5f716370216744e13b9e
Jinseok Yang <49273615+rinyang2@users.noreply.github.com>
2024-01-07 22:25:39 +0900

    [Silver IV] Title: Generations of Tribbles, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.py
+++ 백준/Silver/9507. Generations of Tribbles/Generations of Tribbles.py
@@ -1,5 +1,7 @@
-arr=[1,1,2,4]
-for i in range(4,70):
-    arr.append(arr[i-1]+arr[i-2]+arr[i-3]+arr[i-4])
+arr=[1]*68
+arr[2]=2
+arr[3]=4
+for i in range(4,68):
+    arr[i] = arr[i-4]+arr[i-3]+arr[i-2]+arr[i-1]
 for _ in range(int(input())):
     print(arr[int(input())])

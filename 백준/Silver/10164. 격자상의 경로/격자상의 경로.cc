668ef2db529c75a522713b6cae1f342b17b5140b
Jinseok Yang <49273615+rinyang2@users.noreply.github.com>
2024-01-08 23:37:00 +0900

    [Silver I] Title: 격자상의 경로, Time: 0 ms, Memory: 2020 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#define endl '\n'
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	int N,M,O;
+	cin>>N>>M>>O;
+	vector<vector<int>>v(20);
+	v[0]={1};
+	v[1]={1,1};
+	for(int i = 2; i<20; i++){
+		v[i].push_back(1);
+		for(int j = 0;j<v[i-1].size()-1;j++){
+			v[i].push_back(v[i-1][j]+v[i-1][j+1]);
+		}
+		v[i].push_back(1);
+	}
+	int y,x;
+	if(O==0){
+		y=N-1;
+		x=M-1;
+	}
+	else if(O%M){
+		y=O/M;
+		x=O%M-1;
+	}
+	else{
+		y=O/M-1;
+		x=M-1;
+	}
+	cout<<v[y+x][y]*v[N+M-y-x-2][N-y-1];
+}

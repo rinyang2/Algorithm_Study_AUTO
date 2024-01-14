1315ddc870821673bf7882a7457d4636cc397194
Jinseok Yang <49273615+rinyang2@users.noreply.github.com>
2024-01-14 23:14:44 +0900

    [Platinum IV] Title: 열혈강호, Time: 428 ms, Memory: 6124 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.cc
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define endl '\n'
+using namespace std;
+
+bool dfs(int x, vector<vector<int>> &v, vector<bool> &done, vector<int> &assign){
+	for(auto a: v[x]){
+		if(done[a]) continue;
+		done[a] = true;
+		if(assign[a]==0||dfs(assign[a],v,done,assign)){
+			assign[a]=x;
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+	int N, M;
+	cin>>N>>M;
+	vector<vector<int>> jobs(N+1);
+	vector<int> assign(1001);
+	for(int i = 1; i<= N; i++){
+		int n;
+		cin>>n;
+		while(n--){
+			int temp;
+			cin>>temp;
+			jobs[i].push_back(temp);
+		}
+	}
+	int cnt=0;
+	for(int i = 1; i<=N; i++){
+		vector<bool> done(1001);
+		if(dfs(i,jobs,done,assign)) cnt++;
+	}
+	cout<<cnt<<endl;
+}
